b458dec6454a0d3783b952b0a69e1d0bfb978fcd
Hoang Do <nkhdo94@gmail.com>
2021-01-22 23:11:32 +0700

    content model

---
+++ database/migrations/1611331533300_contents.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Contents extends BaseSchema {
+  protected tableName = 'contents'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.string('slug').notNullable().unique().index()
+      table.text('description')
+      table.jsonb('tags').defaultTo('[]')
+      table.jsonb('content').defaultTo('{}')
+      table.timestamps(true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
