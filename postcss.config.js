5220c8874a7fa2dfd657fd37fcc11e70310e7672
Hoang Do <nkhdo94@gmail.com>
2021-01-23 10:01:30 +0700

    remove comments in cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ module.exports = (ctx) => ({
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    cssnano: ctx.env === 'production' ? {} : false,
+    cssnano:
+      ctx.env === 'production'
+        ? {
+            preset: ['default', { discardComments: { removeAll: true } }],
+          }
+        : false,
   },
 })
