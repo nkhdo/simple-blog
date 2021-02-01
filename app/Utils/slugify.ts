cd8f982ec3fa465ccfe8e06c08460716b977057c
Hoang Do <nkhdo94@gmail.com>
2021-02-01 12:53:25 +0700

    minor update

--- app/Utils/slugify.ts
+++ app/Utils/slugify.ts
@@ -3,8 +3,8 @@ const slugify = (string: string, separator = '-') => {
     .normalize('NFD') // split an accented letter in the base letter and the acent
     .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
     .toLowerCase()
-    .trim()
     .replace(/:.+:/g, '') // remove all emoji texts
+    .trim()
     .replace(/[^a-z0-9\- ]/g, '') // remove all chars not letters, numbers and spaces (to be replaced)
     .replace(/\s+/g, separator)
 }
